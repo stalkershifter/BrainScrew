2972c092ed3f0319154babd6f524d37138bd53ea
Никола Манчић <mn120354d@student.etf.bg.ac.rs>
2021-08-01 18:10:26 +0200

    Add Examples

---
+++ BrainScrew/Parser/BrainScrewParser.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrainScrew.Parser
+{
+    public class BrainScrewParser
+    {
+        const Int32 BufferSize = 128;
+        char[] programCommands = { '>', '<', '+', '-', '.', ',', '[', ']' };
+
+        private String brainScrewFileName;
+        private List<Byte> memoryTape;
+        private int pointer;
+        private Stack<int> loopStack;
+
+        public BrainScrewParser(String fileName)
+        {
+            brainScrewFileName = fileName;
+            loopStack = new Stack<int>();
+            memoryTape = new List<Byte>();
+            memoryTape.Add(0);
+            pointer = 0;
+        }
+
+        
+        public void Compile()
+        {
+
+            using (var fileStream = File.OpenRead(brainScrewFileName))
+            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8, true, BufferSize))
+            {
+                String program;
+                if((program = streamReader.ReadToEnd()) != null)
+                {
+                    int loopPairOpeningBrakets = 0, loopPairClosingBrakets = 0;
+                    bool ignore = false;
+                    for (int programCounter = 0; programCounter < program.Length;)
+                    {
+                        char command = program[programCounter];
+
+                        if (!ignore || command == '[' || command == ']')
+                        {
+                            switch (command)
+                            {
+                                case '>':
+                                    pointer++;
+                                    if (pointer >= memoryTape.Count)
+                                        memoryTape.Add(0);
+                                    break;
+                                case '<':
+                                    pointer--;
+                                    if (pointer < 0)
+                                    {
+                                        Console.WriteLine("Error: Pointer is out of bounds.");
+                                        return;
+                                    }
+                                    break;
+                                case '+':
+                                    memoryTape[pointer]++;
+                                    break;
+                                case '-':
+                                    memoryTape[pointer]--;
+                                    break;
+                                case ',':
+                                    char input = Console.ReadKey().KeyChar;
+                                    memoryTape[pointer] = (byte)input;
+                                    break;
+                                case '.':
+                                    Console.Write((char)memoryTape[pointer]);
+                                    break;
+                                case '[':
+                                    if (memoryTape[pointer] == 0 || ignore)
+                                    {
+                                        ignore = true;
+                                        loopPairOpeningBrakets++;
+                                    }else  
+                                        loopStack.Push(programCounter);
+                                    break;
+                                case ']':
+                                    if (ignore)
+                                    {
+                                        loopPairClosingBrakets++;
+                                        if (loopPairOpeningBrakets == loopPairClosingBrakets)
+                                        {
+                                            loopPairOpeningBrakets = loopPairClosingBrakets = 0;
+                                            ignore = false;
+                                        }
+                                        programCounter++;
+                                    }
+                                    else
+                                        programCounter = loopStack.Pop();
+                                    break;
+                            }
+
+                        }
+
+                        if (command != ']')
+                           programCounter++;
+                    }
+                }   
+            }
+        }
+    }
+}
--- BrainScrew/Program.cs
+++ BrainScrew/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BrainScrew.Parser;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,9 @@ namespace BrainScrew
     {
         static void Main(string[] args)
         {
+            BrainScrewParser brainScrewParser = new BrainScrewParser(@"..\..\Examples\HelloWorld.bs");
+            brainScrewParser.Compile();
+            Console.ReadLine();
         }
     }
 }
